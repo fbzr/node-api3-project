5210e6ae62ed7d05a09a1970a0b172e0d7d06c21
fbzr <fabricio.spb@gmail.com>
2020-04-09 08:46:45 -0700

    add .env file, create start script

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // code away!
+require('dotenv').config();
 const server = require('./server');
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
